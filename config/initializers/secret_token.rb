172b86b95320134c6a518acd8f6d015cee428675
rangerdante <w.garland.long@gmail.com>
2011-04-28 20:00:06 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicAnywhere::Application.config.secret_token = '5c983b5a3e299a409cb35b992fb3941ce64c2433aa7599a879f0df4df8cffb52e1546037f6c78fdf553f36168d10113351fbee8d33d52f0f92d210ff8a9e7896'
