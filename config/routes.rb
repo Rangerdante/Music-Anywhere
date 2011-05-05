01c5b664ae3e51ab4e7c098a4afcf4bdcee4ac9b
rangerdante <w.garland.long@gmail.com>
2011-05-04 21:54:53 -0400

    Fixed links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 MusicAnywhere::Application.routes.draw do
 	match '/login', :to => 'pages#login'
 	match '/about', :to => 'pages#about'
+	match '/signup', :to => 'pages#signup'
 	root :to => 'pages#home'
 end
